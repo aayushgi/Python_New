5040230e46610f053c1f7caef67136f056d8ddc3
ayushgi <gitayush30@gmail.com>
2025-11-15 11:35:36 +0530

    finding quadrant

--- ass02/p4.py
+++ ass02/p4.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+#  Write a python program to take coordinates of a point as input and determine its 
+x= float(input("enter the vale of x coordinate: "))
+y= float(input("enter the value of y coordinate: "))
+
+if x>0 and y>0:
+    print("The point lies in the First Quadrant")
+elif x<0 and y>0:
+    print("The point lies in the Second Quadrant")
+elif x<0 and y<0:
+    print("The point lies in the Third Quadrant")
+elif x>0 and y<0:
+    print("The point lies in the Fourth Quadrant")
+else:
+    print("The point lies on the Axis")    
