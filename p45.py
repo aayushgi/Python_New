b10b28009fc7a62a44af3656ee213625170e5955
ayushgi <gitayush30@gmail.com>
2025-11-24 16:58:43 +0530

    python assignments

---
+++ p45.py
@@ -0,0 +1,5 @@
+# 
+a=[]
+print(type(a))
+b=[10, 20, 30]
+print(type(b))
---
+++ p46.py
@@ -0,0 +1,7 @@
+# wap to take list of your friends
+x=[10, 20, 30, 40]
+x.append(60)
+print(x)
+y=['ayu', 'sss']
+x.extend(y)
+print(x)
