b9e63f028b11d15b2f86b117be0713157aff5ceb
ayushgi <gitayush30@gmail.com>
2025-11-26 11:01:25 +0530

    aasignment 2 q7

--- ass02/p7.py
+++ ass02/p7.py
@@ -1,3 +1,6 @@
 #Write a python program to print table of given number in given format
-num= int(input("enter the number which table did you want "))
-print("table of" , num , "is:")
+n= int(input("enter the number which table did you want "))
+i=1
+while i<=10:
+	print(f"{n}*{i}={n*i}")
+	i=i+1
