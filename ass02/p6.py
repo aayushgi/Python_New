fe0c8150d71851506b6ec4c02ca4b04402d82821
ayushgi <gitayush30@gmail.com>
2025-11-26 10:48:08 +0530

    aasignment 2 q6

--- ass02/p6.py
+++ ass02/p6.py
@@ -1,3 +1,10 @@
-#Write a python program to print table of given number in given format
-num= int(input("enter the number which table did you want "))
-print("table of" , num , "is:")
+#Write a python program to calculate electricity bill based on following parameters:- 
+unit=int(input("enter the units that you have been used: "))
+if unit>1 and unit<=150:
+	bill=unit*2.40
+elif unit>150 and unit<=300:
+	bill=(150*2.40)+(unit-150)*3
+elif unit>300:
+	bill=(150*2.40)+(150*3)+(unit-300)*3.20
+print("total unit used",unit,
+"your bill",bill)
