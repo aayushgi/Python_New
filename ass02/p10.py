8ce1a6c5d647bb44313910d0cc22c96a68a6c1b0
ayushgi <gitayush30@gmail.com>
2025-11-27 21:32:05 +0530

    reverse of the number

---
+++ ass02/p10.py
@@ -0,0 +1,10 @@
+# Write a python program to reverse the digits of given number. 
+num=int(input("enter the the number you want to reverse "))
+temp=0
+n=num
+while n>0:
+	
+	rem=n%10
+	temp=temp*10+rem
+	n=n//10
+print("the reverse of the",num,"is",temp)
