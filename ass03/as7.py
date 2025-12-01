2c4d2b2e7744ba8249fb3dbea55c4a2087faf2eb
ayushgi <gitayush30@gmail.com>
2025-12-01 11:56:38 +0530

    ass3 quest7

---
+++ ass03/as7.py
@@ -0,0 +1,11 @@
+"""
+Write a python program to find number of occurrences of each vowel present in the 
+given string? """
+student = {
+    "name": "Ayush",
+    "age": 20,
+    "course": "CSE"
+}
+print("dictonary elements: ")
+for key, value in student.items():
+	print(key,"=",value)
