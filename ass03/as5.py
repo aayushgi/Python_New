7e95443574b21e7bd177703db050e9aee470a494
ayushgi <gitayush30@gmail.com>
2025-12-01 11:29:38 +0530

    ass3 quest5

---
+++ ass03/as5.py
@@ -0,0 +1,8 @@
+""" Write a python program to create a tuple of five numbers. Now apply len(), max(), 
+min() and sum() functions on tuple. """
+name=(52,12,22,56,85)
+print("tuple member",name)
+print("length of tuple: ",len(name))
+print("max in in the tuple: ",max(name))
+print("min in the tuple: ",min(name))
+print("sum of the tuple: ",sum(name))
