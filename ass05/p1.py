56d7a65fc02f1777b45bcd6df327b94db1428bb1
ayushgi <gitayush30@gmail.com>
2025-12-04 10:29:00 +0530

    ques1

---
+++ ass05/p1.py
@@ -0,0 +1,9 @@
+"""
+Write a python program to create a class with name ‘MyClass’. In MyClass create a 
+function sayHello(). In sayHello() function pass name of user, and display user name 
+with Hello, message. Test class ‘MyClass’. """
+class MyClass:
+	def sayHello(self):
+		print("hellow, ayush")
+c=MyClass()
+c.sayHello()
