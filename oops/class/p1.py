6de570bb23029eaed1428c6845f5e73bed12a7f1
ayushgi <gitayush30@gmail.com>
2025-12-16 11:02:09 +0530

    opps

---
+++ oops/class/p1.py
@@ -0,0 +1,9 @@
+class myclass:
+    def sayhello(self,n):
+
+        print("hello ",n)
+
+name=input("enter your name here: ")
+m=myclass()#create object
+m.sayhello(name)#callin function of the class
+
