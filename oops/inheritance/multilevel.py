c0c3da8314e6afeb3c7fd6e0eb2e5fe09b9e54b0
ayushgi <gitayush30@gmail.com>
2025-12-18 18:36:15 +0530

    multilevel  inheritance

---
+++ oops/inheritance/multilevel.py
@@ -0,0 +1,29 @@
+#this program is about multilevel inheritance
+class Employee:
+    def setEmployee(self, empid, empname):
+        self.empid = empid
+        self.empname = empname
+    def getEmployee(self):
+        print("employee id: ", self.empid)
+        print("employee name: ", self.empname)
+class payroll(Employee):
+    def setpayroll(self, bs, hra, da):
+        self.bs=bs
+        self.hra=hra
+        self.da=da
+    def getpayroll(self):
+        print("basic salary: ", self.bs)
+        print("HRA is: ", self.hra)
+        print("DA is: ",self.da)
+class payslip(payroll):
+    def netsalary(self):
+        netsal=self.bs + self.hra + self.da
+        print("net salary is: ",netsal)
+eid=int(input("enter employee id here: "))
+ename=input("enter employee name here: ")
+b=float(input("enter your basic salary here: "))
+h=float(input("enter your HRA here: "))
+d=float(input("enter your DA here: "))
+ps=payslip()
+ps.setEmployee(eid, ename)
+ps.setpayroll(b,h,d)
