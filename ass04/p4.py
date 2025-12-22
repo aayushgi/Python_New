b5b33b11c9553cd78bc6198f31e740c66915e1e8
ayushgi <gitayush30@gmail.com>
2025-12-22 12:10:06 +0530

    assignment 4

--- ass04/p4.py
+++ ass04/p4.py
@@ -2,5 +2,11 @@
 pass a string and substring. If substring is presented in string then return true or 
 return false.
 """
-#not aading code today
-#no files to be added
+def substr(string, substring):
+    if substring in string:
+        return True
+    else: 
+        return False
+string=input("enter the string here: ")
+substring=input("enter the substring here: ")
+print(substr(string, substring))
