b2dba6cb7c86d95f7cc75fcda8869e0cca4bbf57
ayushgi <gitayush30@gmail.com>
2025-12-22 12:00:31 +0530

    assignment 4

--- ass04/p1.py
+++ ass04/p1.py
@@ -1,7 +1,12 @@
 """Write a python program to create two functions area() and peri() to find area and 
 perimeter of rectangle. """
 l=int(input("enter the length of the of the rectangle: "))
-b=int(input("enter the breath of the rectangle"))
-def area(l,b):
-	if l>0 and b>0:
-		return()
+b=int(input("enter the breath of the rectangle: "))
+def area(length,breadth):
+	return length*breadth
+def peri(length,breadth):
+	return 2*(length+breadth)
+print("area of rectangle is: ",area(l,b))
+print("perimeter of rectangle is:", peri(l,b))
+
+	
