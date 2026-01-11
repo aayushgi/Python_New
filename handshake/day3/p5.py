ca548aeb7e12139f9491bf21b7d4a6ae483b8029
ayushgi <gitayush30@gmail.com>
2026-01-11 10:34:43 +0530

    handshake day3

---
+++ handshake/day3/p5.py
@@ -0,0 +1,21 @@
+#Use logical operators to check multiple conditions.
+#using and operator
+age= int(input("Enter your age: "))
+marks=int(input("Enter your marks: "))
+if age>=18 and marks>=80:
+    print("Eligible for scholarship")
+else:
+    print("Not eligible for scholarship")
+#using or operator
+username = "ayush"
+email_verified = False
+phone_verified = True
+
+if email_verified or phone_verified:
+    print("User verification successful")
+else:
+    print("User verification failed")
+#using not operator
+is_raining = False
+if not is_raining:
+    print("It's a sunny day, go outside!")  
