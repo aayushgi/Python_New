3e09a0e4607977ee4c7a99285f587239988382b0
ayushgi <gitayush30@gmail.com>
2026-01-18 22:24:52 +0530

    added p6.py

---
+++ handshake/day3/p6.py
@@ -0,0 +1,8 @@
+""""PS C:\spi\random program> git push origin master
+>>
+fatal: 'origin' does not appear to be a git repository
+fatal: Could not read from remote repository.
+
+Please make sure you have the correct access rights
+and the repository exists.
+PS C:\spi\random program>"""""
