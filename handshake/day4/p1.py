aad38fef005fb8f7463e923ca90c0ecd27f0e375
ayushgi <gitayush30@gmail.com>
2026-01-20 20:02:17 +0530

    handshake day4

---
+++ handshake/day4/p1.py
@@ -0,0 +1,8 @@
+#Check whether a number is positive, negative, or zero
+n=int(input("enter the number here: "))
+if n>0:
+    print("this is a positive number")
+elif n<0:
+    print("this number is negative")
+else:
+    print("number is zero")
