aa75afbe3a636c5734dd9c64453b7fb1afef3e82
ayushgi <gitayush30@gmail.com>
2026-01-20 19:42:12 +0530

    handshak day3

---
+++ handshake/day3/p10.py
@@ -0,0 +1,5 @@
+#Display formatted details using f-strings
+#f string ka mtalab hota hai formatting string (Text ke beech me variable ki value dikhana.)
+name="ayush saxena"
+age=23
+print(f"my name is {name} and my age is {age}")
