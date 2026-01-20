f5820d71b65d771675196b4bf27b37e043b5a6c6
ayushgi <gitayush30@gmail.com>
2026-01-20 19:34:32 +0530

    handshak day3

--- handshake/day3/p9.py
+++ handshake/day3/p9.py
@@ -1,1 +1,7 @@
-#this is about day 3 handshake
+
+#Print multiplication table of user input
+n=int(input("enter the number which table did you want: "))
+i=1
+while(i<=10):
+    print(n,"*",i,"=",(i*n))
+    i=i+1
