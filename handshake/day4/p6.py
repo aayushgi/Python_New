6aa3c046cef302f6e7583efebb514129afc21554
ayushgi <gitayush30@gmail.com>
2026-01-21 17:23:27 +0530

    handshake day4

---
+++ handshake/day4/p6.py
@@ -0,0 +1,6 @@
+#Check voting eligibility using if-else
+age=int(input("enter your age here: "))
+if age>18:
+    print("you are eligibile for vote")
+else:
+    print("you are not eligible for vote")
