a2657c861c39fa773c1f5896d89b4b0454e3dd60
ayushgi <gitayush30@gmail.com>
2026-01-22 21:15:28 +0530

    handshake day5

---
+++ handshake/day5/p2.py
@@ -0,0 +1,8 @@
+
+#Function to return average of three numbers.
+n1=int(input("enter frist number here:"))
+n2=int(input("enter second number here:"))
+n3=int(input("enetr third number here:"))
+def average(n1,n2,n3):
+    return (n1+n2+n3)/3
+print("the average of number is ",average(n1,n2,n3))
