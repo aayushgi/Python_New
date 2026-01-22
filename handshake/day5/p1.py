1f307ec8f739778ff60212b3f612e81c64889ecc
ayushgi <gitayush30@gmail.com>
2026-01-22 17:54:09 +0530

    handshake day5

--- handshake/day5/p1.py
+++ handshake/day5/p1.py
@@ -1,1 +1,6 @@
 #Create function to calculate area of rectangle
+l=int(input("enter the length of the rectangle: "))
+b=int(input("enter the breath of the rectangle: "))
+def rectangle(l,b):
+    return l*b
+print("area of rectangle= ",rectangle(l,b))
