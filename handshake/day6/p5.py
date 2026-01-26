1fbd62782f43175abf750fb278733239af43e5b1
ayushgi <gitayush30@gmail.com>
2026-01-26 22:21:47 +0530

    handshake day6

--- handshake/day6/p5.py
+++ handshake/day6/p5.py
@@ -1,18 +1,8 @@
 
 #Create dictionary storing name, roll, marks.
 student={
-   1:{ "name":"aryan",
-    "roll":2343,
-    "marks":25},
+   1:{ "name":"aryan","roll":2343,"marks":25},
 
-   2:{
-      "name":"aman",
-    "roll":2349,
-    "marks":200
-},
-3:{
-    "name":"aan",
-    "roll":243,
-    "marks":125
-}}
+   2:{"name":"aman","roll":2349,"marks":200},
+3:{"name":"aan","roll":243, "marks":125}}
 print(student)
