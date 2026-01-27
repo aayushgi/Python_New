883aea5bfcf7d72a47f4be6cbe622269ea22f6e6
ayushgi <gitayush30@gmail.com>
2026-01-27 23:23:19 +0530

    handshake day6

---
+++ handshake/day6/p8.py
@@ -0,0 +1,21 @@
+
+#Create nested dictionary for 3 students.
+students={
+    "student1":{
+        "name":"ayush",
+        "roll number":12,
+        "marks":90
+    },
+    "student2":{
+        "name":"aryan",
+        "roll number":13,
+        "marks":-90
+    },
+    "student3":{
+        "name":"muskan",
+        "roll number":16,
+        "marks":-100
+
+    }
+}
+print(students)
