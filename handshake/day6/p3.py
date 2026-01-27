4a767019202a9f8aaf9dc2a70ee5af1a2be0f077
ayushgi <gitayush30@gmail.com>
2026-01-27 22:39:07 +0530

    handshake day6

---
+++ handshake/day6/p3.py
@@ -0,0 +1,10 @@
+
+#Demonstrate tuple packing and unpacking.
+#packing
+student="ayush", 90 ,"A"
+print(student)
+#unpacking
+name,marks,grade=student
+print("name:",name)
+print("marks:",marks)
+print("grade:",grade)
