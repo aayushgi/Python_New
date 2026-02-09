c118c0a19ef396d62b2e82cf89dbad74338ec427
ayushgi <gitayush30@gmail.com>
2026-02-09 16:08:56 +0530

    handsahke

---
+++ handshake/day9/p8.py
@@ -0,0 +1,6 @@
+#
+#List all installed packages using pip list.
+"""
+used to check all the installed packages in pyhton.
+we have to write this on our terminal or on our command prompt
+python -m pip list"""
