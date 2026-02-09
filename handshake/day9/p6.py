ac23ecd4d0714786b76113db20ca2eb168111f28
ayushgi <gitayush30@gmail.com>
2026-02-09 15:22:51 +0530

    handsahke

---
+++ handshake/day9/p6.py
@@ -0,0 +1,7 @@
+#Use math module to calculate square root and area.
+import math
+num=int(input("enter an number here: "))
+square=math.sqrt(num)
+print("the sqyuareroot of the number is ",square)
+area=math.pi*num*num
+print("the area is ",area)
