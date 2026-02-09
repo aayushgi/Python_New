872f4f42c8e806efe9faa1e3cbcecf44d3b20af8
ayushgi <gitayush30@gmail.com>
2026-02-09 16:04:36 +0530

    handsahke

--- handshake/day9/p7.py
+++ handshake/day9/p7.py
@@ -1,5 +1,5 @@
 #Install requests package and fetch a webpage.
 import requests
 response=requests.get("https://github.com/")#link
-print("status ",response.status_code)
+print("status ",response.status_code)#if 200 then success
 print(response.text[:300])#number of text
