f270828b0dfab65e56e2f55931ff62026318f638
ayushgi <gitayush30@gmail.com>
2026-02-09 15:16:58 +0530

    handsahke

---
+++ handshake/day9/p5.py
@@ -0,0 +1,6 @@
+#this program is about to generating an random number
+#
+#Use random module to generate random numbers.
+import random
+num=random.randint(1,100)
+print("here is the random number between 1 to 100:",num)
