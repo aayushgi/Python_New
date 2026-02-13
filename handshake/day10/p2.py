ca672f660900278673f402b4e1ffa1ef3adbe99f
ayushgi <gitayush30@gmail.com>
2026-02-13 22:47:07 +0530

    yeild function

--- handshake/day10/p2.py
+++ handshake/day10/p2.py
@@ -1,1 +1,10 @@
 #Write generator yielding square of numbers.
+def square_generator(n):
+    for i in range(1, n + 1):
+        yield i * i
+
+# Using the generator
+gen = square_generator(5)
+
+for value in gen:
+    print(value)
