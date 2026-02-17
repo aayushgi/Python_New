51c997a69643fb3498f2a8a5daf1917e11f3ce92
ayushgi <gitayush30@gmail.com>
2026-02-17 15:44:27 +0530

    handshake

---
+++ handshake/day11/p2.py
@@ -0,0 +1,8 @@
+#Perform element-wise addition and multiplication.
+import numpy as np
+a=np.array([1,2,3])
+b=np.array([4,5,6])
+addition=a+b
+print("addition is",addition)
+multiplication=a*b
+print("multiplication is",multiplication)
