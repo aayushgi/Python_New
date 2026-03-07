0bc546d6e844b2f3f37fe77b482c2653150a0515
ayushgi <gitayush30@gmail.com>
2026-03-07 16:56:45 +0530

    numpy

--- handshake/day11/p9.py
+++ handshake/day11/p9.py
@@ -1,1 +1,16 @@
 #Multiply two matrices using np.dot().
+import numpy as np
+A=np.array([
+    [1,2,3],
+    [4,5,6]
+
+])
+B=np.array([
+    [12,13,14],
+    [14,15,16],
+    [17,70,27]
+])
+multilpy=np.dot(A,B)
+print("matrix A:\n",A)
+print("matrix B:\n",B)
+print("matrix dot product A.B:\n",multilpy)
