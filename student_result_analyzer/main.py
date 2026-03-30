79d04b4a0313888089325b8f051c0a2f5d46790e
ayushgi <gitayush30@gmail.com>
2026-03-31 00:21:10 +0530

    result analuzer==ADDING REPORT CARD

--- student_result_analyzer/main.py
+++ student_result_analyzer/main.py
@@ -1,3 +1,4 @@
+
 import json
 import os
 students=[]
@@ -59,6 +60,74 @@ def compute_result():
     print("result calculated")
 
 
+def search_by_grade(grade):
+    found=False
+    for s in students:
+        if s.get("grade")==grade:
+            print("---------------------------------------------------")
+            print("roll number",s["roll"])
+            print("name",s["name"])
+            print("grade",s.get("grade"))
+            print("percentage",s.get("percentage"))
+            found=True
+    if not found:
+        print("no student with this grade")
+
+
+def subject_topper_report():
+    subjects=["python","maths","DBMS","OS"]
+    for sub in subjects:
+        topper=max(students,key=lambda s:s["marks"].get(sub,0))
+        print("-----------------------------------------------------------")
+        print(sub,"topper")
+        print("name: ",topper["name"])
+        print("marks",topper["marks"].get(sub,0))
+
+def export_report_card(stu):
+    import os
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    templates_dir = os.path.join(base_dir, "templates")
+    os.makedirs("templates", exist_ok=True)
+    filename=f"templates/report_{stu['roll']}.txt"
+    with open(filename,"w") as file:
+        file.write("===========STUDENT REPORT CARD============\n\n")
+        file.write(f"roll no. :{stu['roll']}\n")
+        file.write(f"name :{stu['name']}\n")
+        file.write(f"class :{stu['class']}\n\n")
+
+        file.write("-----------marks-------------")
+
+        total=0
+        count = 0
+        
+        for subject, marks in stu["marks"].items():
+            file.write(f"{subject}: {marks}\n")
+            total += marks
+            count += 1
+        
+        
+        avg = total / count if count > 0 else 0
+        
+        file.write("\n-----------------\n")
+        file.write(f"Total Marks : {total}\n")
+        file.write(f"Average     : {avg:.2f}\n")
+        
+        
+        if avg >= 90:
+            grade = "A+"
+        elif avg >= 75:
+            grade = "A"
+        elif avg >= 60:
+            grade = "B"
+        elif avg >= 50:
+            grade = "C"
+        else:
+            grade = "F"
+        
+        file.write(f"Grade       : {grade}\n")
+    
+    print(f"Report card exported successfully to {filename}")
+
 def list_students():
     for s in students:
         print("-------------------------------------------------------")
@@ -88,6 +157,15 @@ def load_json():
     except FileNotFoundError:
         print("file not found")
 
+def delete_student():
+    roll=input("enter roll number you want delete")
+    for s in students:
+        if s.get("roll")==roll:
+            students.remove(s)
+            print("student removed successfully")
+            return
+    print("student not found")
+
 while True:
     print("\n----- ---------------student result analyzer-----------------------")
     print("1 to add student")
@@ -96,6 +174,10 @@ while True:
     print("4 to list students")
     print("5 to save json")
     print("6 to load json")
+    print("7 to delete student")
+    print("8 to find student with their grade")
+    print("9 to get subject topper list")
+    print("10 to export report card")
     print("0 for exit")
 
 
@@ -114,6 +196,21 @@ while True:
         save_json()
     elif choice=="6":
         load_json()
+    elif choice=="7":
+        delete_student()
+    elif choice=="8":
+        grade=(input("enter the grade here"))
+        search_by_grade(grade)
+    elif choice=="9":
+        subject_topper_report()
+    elif choice=="10":
+        roll = input("Enter roll number: ")
+        for s in students:
+            if s["roll"] == roll:
+                export_report_card(s)
+                break
+        else:
+            print("Student not found")
     elif choice=="0":
         break
 
